955a4ad3faa418a2934c61d81c8a94d3fb49ecbd
Onur Kılınç <onur81730@gmail.com>
2026-02-14 17:16:19 +0300

    index.html yolu ve App.jsx ismi düzeltildi

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './pages/Home.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app.jsx"; // senin app.jsx dosyan
+import "./index.css";
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
